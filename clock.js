dce3c58e634c104019f4737e118fb7a375fefec2
Richa Sharma <richasharma@Richas-MacBook-Air.local>
2018-08-19 20:25:45 +0530

    Added files for clock

---
+++ clock.js
@@ -0,0 +1,35 @@
+/* JS File for Digital Clock
+Author: Richa Sharma,
+Creation Date: 30 July 2018
+*/
+
+
+setInterval(displayclock , 500);
+function displayclock () {
+	var time = new Date ();
+	var hrs = time.getHours();
+	var min = time.getMinutes();
+	var sec = time.getSeconds();
+
+
+
+	if (hrs > 12) {
+		hrs = hrs - 12;
+	}
+	if ( hrs == 0) {
+		hrs = 12;
+	}
+	if (hrs < 10) {
+		hrs = '0' + hrs;
+	}
+	if (min < 10) {
+		min = '0' + min;
+	}
+	if (sec < 10) {
+		sec = '0' + sec;
+	}
+
+	document.getElementById('clock').innerHTML = hrs + ':' + min + ':' + sec;
+
+
+}
